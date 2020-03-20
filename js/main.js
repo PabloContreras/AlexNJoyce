c31bcfa5215b21fa7409e6e6d630ebdc8320ebcc
pablocontreras <pablo_contreras_1997@outlook.com>
2020-03-19 19:42:02 -0600

    timer

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("31 October 2020 12:00:00 GMT-07:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
